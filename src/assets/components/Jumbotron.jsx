45f13675901650e1355ef007b684e8b1f13d1360
Afif Al Dzakwan <aldzaqwan112@gmail.com>
2025-04-08 18:12:02 +0700

    Update

--- src/assets/components/Jumbotron.jsx
+++ src/assets/components/Jumbotron.jsx
@@ -11,7 +11,7 @@ const Jumbotron = () => {
             const fetchMovies = async () => {
                 try {
                     const result = await getMovieNowPlaying();
-                    setMovieNow(result.slice(0, 5)); // Get the first 5 movies
+                    setMovieNow(result.slice(0, 15)); 
                 } catch (err) {
                     console.error("Error fetching movie list:", err);
                 }
@@ -30,14 +30,26 @@ const Jumbotron = () => {
         const currentMovie = moviesNow[current];
 
   return (
-    <article className='text-white w-full min-h-screen bg-cover' style={{
+    <article className='text-white w-full min-h-[40vw] bg-black/50 bg-blend-multiply bg-cover' style={{
         backgroundImage: currentMovie
           ? `url(https://image.tmdb.org/t/p/original${currentMovie.backdrop_path})`
           : 'none',
     }}>
-        <div className="container" >
-            
+        <div className="container p-20 flex items-end  relative" >
+            <div className=''>
+                <h1 className='text-5xl font-bold'>{currentMovie ? currentMovie.title : 'Loading...'}</h1>
+                <p className='text-lg mt-4 w-[40%]'>{currentMovie ? currentMovie.overview : 'Loading...'}</p>
+                <button className='mt-6 bg-gradient-to-b from-orange-500 via-red-500 to-yellow-500 text-white px-4 py-2 rounded'>Watch Now</button>
+                <div className='mt-10'>
+            {moviesNow.map((movie, index) => (
+                <span key={index} onClick={() => setCurrent(index)} className={`inline-block w-2 h-2 rounded-full mx-1 ${current === index ? 'bg-red-600' : 'bg-gray-400'}`}>
+
+                </span>
+            ))}
         </div>
+            </div>
+        </div>
+        
         
     </article>
   )
